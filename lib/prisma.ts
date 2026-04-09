8e39e483d3443cb0261ec1ec2c291e59b4beace0
Hans Naperi <164773533+hanzekel@users.noreply.github.com>
2026-04-09 20:02:43 +0800

    Normalize Prisma setup for deployment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,11 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = process.env.DATABASE_URL!;
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-const adapter = new PrismaPg({
-  connectionString,
-});
-
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient({ adapter });
+export const prisma = global.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  global.prisma = prisma;
 }
